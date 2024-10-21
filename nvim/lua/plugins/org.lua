23a20f9c748b236c203130c1c423e08f712a655f
dominiclet <dominic.let@hotmail.com>
2024-10-21 23:11:33 +0800

    changes to org

--- nvim/lua/plugins/org.lua
+++ nvim/lua/plugins/org.lua
@@ -1,19 +1,20 @@
 return {
-  'nvim-orgmode/orgmode',
-  event = 'VeryLazy',
-  ft = { 'org' },
-  config = function()
-    -- Setup orgmode
-    require('orgmode').setup({
-      org_agenda_files = '~/notes/**/*',
-      org_default_notes_file = '~/notes/refile.org',
-    })
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+            org_agenda_files = '~/notes/**/*',
+            org_default_notes_file = '~/notes/refile.org',
+            org_startup_indented = true
+        })
 
-    -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-    -- add ~org~ to ignore_install
-    -- require('nvim-treesitter.configs').setup({
-    --   ensure_installed = 'all',
-    --   ignore_install = { 'org' },
-    -- })
-  end,
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        -- require('nvim-treesitter.configs').setup({
+        --   ensure_installed = 'all',
+        --   ignore_install = { 'org' },
+        -- })
+    end,
 }
