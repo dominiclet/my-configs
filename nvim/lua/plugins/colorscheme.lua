699f2290b6cb083ff43a1f5bc12b70d75ac4ee08
dominiclet <dominic.let@hotmail.com>
2024-10-27 20:06:59 +0800

    update colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,8 @@
 return {
-    "sainnhe/gruvbox-material",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.gruvbox_material_background = "medium";
-        vim.g.gruvbox_material_diagnostic_virtual_text = 'colored';
-        vim.cmd.colorscheme("gruvbox-material")
+        vim.cmd.colorscheme("kanagawa-wave")
     end,
 }
