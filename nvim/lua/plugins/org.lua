e2693eb423d6973c8d0e4cf0cb4d20e97fbe13fa
dominiclet <dominic.let@hotmail.com>
2024-10-27 21:29:31 +0800

    fix S-CR in org

--- nvim/lua/plugins/org.lua
+++ nvim/lua/plugins/org.lua
@@ -9,11 +9,12 @@ return {
             org_default_notes_file = '~/notes/refile.org',
             org_startup_folded = "content"
         })
+
         vim.api.nvim_create_autocmd('FileType', {
             pattern = 'org',
             callback = function()
                 vim.keymap.set('i', '<S-CR>', '<cmd>lua require("orgmode").action("org_mappings.meta_return")<CR>', {
-                    silent = true,
+                    silent = false,
                     buffer = true,
                 })
             end,
