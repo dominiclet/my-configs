08cc94982a342566be75440c2ba17cffc7497866
Dominic <dominic.let@hotmail.com>
2024-12-28 17:46:51 +0800

    telescope ignore node_modules

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,6 +14,9 @@ return {
                         ["<C-d>"] = actions.delete_buffer
                     },
                 },
+                file_ignore_patterns = {
+                    "node_modules"
+                }
             }
         }
 
