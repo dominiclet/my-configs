e97ea9bd36655f7e6efdeafd956a567d90b32f69
Dominic <dominic.let@hotmail.com>
2026-06-29 23:17:44 +0800

    add conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                go = { "gofmt", "goimports" }
+            }
+        })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            callback = function(args)
+                require("conform").format({ bufnr = args.buf })
+            end,
+        })
+    end
+}
